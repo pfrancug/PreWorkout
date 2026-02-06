9b8914bb3ea445312a94c8ba1e0b788b7a346813
Piotr Francug <piotr.francug@icloud.com>
2026-02-06 01:25:50 +0100

    fix: adjust max width of DiaryPage for improved layout

--- src/pages/DiaryPage.tsx
+++ src/pages/DiaryPage.tsx
@@ -12,7 +12,7 @@ export const DiaryPage = () => {
   const columns = useMemo(() => getColumns(t), [t]);
 
   return (
-    <div className={'mx-auto w-full max-w-4xl flex flex-1 flex-col gap-8 p-6'}>
+    <div className={'mx-auto w-full max-w-3xl flex flex-1 flex-col gap-8 p-6'}>
       <div className={'space-y-1'}>
         <h1 className={'text-3xl font-bold tracking-tight'}>
           {t('diary.title')}
