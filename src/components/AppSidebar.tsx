a0859856448f21fd3d4ffc5e99dd738e6738387a
Piotr Francug <piotr.francug@icloud.com>
2026-02-08 22:07:36 +0100

    feat: update favicon files and improve icon size in AppSidebar component

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -131,7 +131,7 @@ export const AppSidebar = () => {
                     'flex aspect-square size-8 items-center justify-center rounded-lg bg-gradient-to-br from-yellow-500 to-red-500 text-white'
                   }
                 >
-                  <Dumbbell className={'size-5'} />
+                  <Dumbbell className={'size-6'} />
                 </div>
 
                 <div className={'grid flex-1 text-left text-sm leading-tight'}>
